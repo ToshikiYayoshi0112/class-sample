602fc37a86e601e8ba93192ae8b7f8500fee9ac3
Yayoshi <sagauyayoshi@gmail.com>
2019-05-07 20:27:56 +0900

    インスタンス変数とインスタンスメソッドについて学んだ。クラスのselfについての理解が よくわからない。インスタンス変数も変数と同じように考える?

--- user.py
+++ user.py
@@ -5,10 +5,23 @@ class User:
         self.age = age
         self.country = country
 
+    def display_profile(self):
+        # display_profile()は、Userクラスのインスタンスメソッド
+        print(f"{self.name}国籍: {self.country} {self.age}歳です。")
+
+    def chenge_nationality(self, country_name):
+        self.country = country_name
+
 
 if __name__ == "__main__":
     bob = User("Bob", 15, "USA")  # Userクラスをインスタンス化
+    bob.chenge_nationality("Chine")
+    bob.display_profile()
 
     tom = User("Tom", 57, "USA")
+    tom.chenge_nationality("Australia")
+    tom.display_profile()
 
     ken = User("Ken", 49, "JP")
+    ken.chenge_nationality("Korea")
+    ken.display_profile()
